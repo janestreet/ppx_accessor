365993201ad4423f3bec12a7dd9239072a5945ef
Arseniy Alekseyev <aalekseyev@janestreet.com>
2021-06-18 20:08:54 +0100

    v0.15~preview.124.35+330

--- src/name.ml
+++ src/name.ml
@@ -8,5 +8,5 @@ let to_constructor_string = Fn.id
 
 let to_lowercase_string t =
   let string = String.lowercase t in
-  if Caml.Hashtbl.mem Lexer.keyword_table string then string ^ "_" else string
+  if Stdlib.Hashtbl.mem Lexer.keyword_table string then string ^ "_" else string
 ;;
